bd1d45ad40c0ba54a58a1e2f64103ed6ce11107d
atif9 <mohammadatif92080@gmail.com>
2022-10-05 14:53:33 +0530

    Added SignUp page

--- LoginPageScript.js
+++ LoginPageScript.js
@@ -30,3 +30,7 @@ document.getElementById("loginBtn").onclick = function(){
         }
     } )
 }
+
+document.getElementById("signUpBtn").onclick = function(){
+    window.location.href = "http://127.0.0.1:5500/SignUpPage.html";
+}
